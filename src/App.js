7f776d7f95e64ef6998548061ab7c2d56cb8abf1
ezeanic <ezeanic@uindy.edu>
2020-12-10 16:09:52 -0500

    finished and open to modification

--- src/App.js
+++ src/App.js
@@ -1,130 +1,76 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Say from 'react-say';
 import './App.css';
-import sessone from './sessone';
-import sesstwo from './sesstwo';
-import Nav from './Nav';
-import sessthree from './sessthree';
-import sessfour from './sessfour';
-import sessfive from './sessfive';
-import Client from './client'
-import Master from './master'
-import Gradebook from './Gradebook';
-import certificate from './certificate';
-import {BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
-import ReactDOM from 'react';
+import SpeechRecognition, { useSpeechRecognition, SpeechSynthesisUtterance } from 'react-speech-recognition';
 
-
-class App extends React.Component{
-  constructor() {
-    super();
-
-    this.state = {
-      Appstyle: {filter: "blur(100px)", margin: '20px', textAlign: 'center'} ,
-      Pagename: "",
-      formdisplay: "block",
-      sent: false,
-      data:["", "", "", ""],
-    };
-    
-    this.Removeoverlay = this.Removeoverlay.bind(this);
-    this.Change = this.Change.bind(this);
-  }
+const App = () => {
+ const [message, setMessage] = useState('');
+ const commands = [
+   {
+     command: 'reset',
+     callback: () => resetTranscript()
+   },
+   {
+     command: 'shut up',
+     callback: () => setMessage('I wasn\'t talking.')
+   },
+   {
+     command: 'Hello',
+     callback: () => setMessage('Hi there!')
+   },
+ ]
+ const command =[]
+ const {
+   transcript,
+   interimTranscript,
+   finalTranscript,
+   resetTranscript,
+   listening,
+ } = useSpeechRecognition({ commands });
   
-  Removeoverlay(){
-    let fname = document.getElementById("form-fname").value;
-    let lname = document.getElementById("form-lname").value;
-    let email = document.getElementById("form-email").value;
-    let email_domain = email.split("@");
 
-    if(email.replace(/[^@]/g, "").length === 1 && email_domain[1].replace(/[^.]/g, "").length >= 1 && email_domain[1].length > 1 && email_domain[0].length > 0){
-      if(fname !== "" && lname !== "" && email !== ""){
-        this.setState({
-          Appstyle: {margin: '20px', textAlign: 'center'}, 
-          Pagename: fname + " " + lname,
-          formdisplay: "none",
-        });
-      }else{
-        alert("empty");
-        return false;
-        throw("Invalid");
-      }
-    }
-    else{
-      throw("Invalid");
-    }
-    
-  };
+ const listenContinuously = () => {
+   SpeechRecognition.startListening({
+     continuous: true,
+   });
+ };
+ return (
+   <div>
+     <div>
+       <span>
+         Listening:
+         {listening ? ' on' : ' off'}
+       </span>
+       <div>
+         
+        <div class="button" id="button-2" onClick={resetTranscript}>
+            <div id="slide"></div>
+          <a href="#" >Reset</a>
+        </div>
 
-  Change(e) {
-    let a = this.state.data;
-    if(e.target.id == "form-fname"){
-      a[0] = e.target.value;
-    }
-    else if(e.target.id == "form-lname"){
-      a[1] = e.target.value;
-    }
-    else if(e.target.id == "form-email"){
-      a[2] = e.target.value;
-    }
-    else{
-      a[3] = e.target.value;
-    }
+        <div class="button" id="button-2" onClick={listenContinuously}>
+          <div id="slide"></div>
+          <a href="#" >Listen</a>
+        </div>
 
-    this.setState({
-      data: a
-    });
-  };
-  
-  render(){
-    return(
-      <Router>      
-        <Nav/>
-        <Switch>
-          <Route path = '/' exact component = {Home}/>
-          <Route path = '/sessone' component = {sessone}/>
-          <Route path = '/sesstwo' component = {sesstwo}/>
-          <Route path = '/sessthree' component = {sessthree}/>
-          <Route path = '/sessfour' component = {sessfour}/>
-          <Route path = '/sessfive' component = {sessfive}/>
-          <Route path = '/Gradebook' component = {Gradebook}/>
-          <Route path = '/certificate' component = {certificate}/>
-          <Route path = '/client' component = {Client}/>
-          <Route path = '/master' component = {Master}/>
-        </Switch>
-      </Router>
-    );
-  }
-}
-
-class Home extends React.Component{
-
-  render(){
-    var links = ['sessone', '/sesstwo', '/sessthree', '/sessfour', '/sessfive'];
-    var aboutheadertext = ['Understanding The Population With Special Needs', 'Engaging Individuals With Special Needs', 'Providing A Great Environment & Experience', 'Engaging Individuals With Special Needs', 'Providing A Great Environment & Experience'];
-    var abouttext = ['Muscular dystrophy, multiple sclerosis, chronic asthma, epilepsy, etc.',
-    'Down syndrome, autism, dyslexia, processing disorders',
-    'ADD, bi-polar, oppositional defiance disorder, etc.',
-    'Blind, visually impaired, deaf, limited hearing',
-    'Engaging properly with best practices with the special needs community.'    
-  ];
-
-
-    var items = [];
-    var sessions = ['Physical', 'Developmental', 'Behavioral/Emotional', 'Sensory Impaired', 'Engagement'];
-
-    for (const [index, value] of links.entries()) {
-      items.push(<div key={index.toString()} ><h1>{aboutheadertext[index]}</h1><p>{abouttext[index]}</p><Link id={"sess"+[index]} className = "navlist" key="index" type="submit" to={value}><li>{sessions[index]}</li></Link><br/><br/><br/><br/></div>)
-    }
-    return(
-          <div id="homepagedashboard">
-            <div id="home">
-              {items}
-            </div>
-          </div>
-    );
-  }
-}
+        <div class="button" id="button-2" onClick={SpeechRecognition.stopListening}>
+          <div id="slide"></div>
+          <a href="#" >Stop</a>
+        </div>
+         {/* <button type="button" onClick={Speaks}>Speak</button> */}
+       </div>
+     </div>
+     <div>
+       <span>{message}</span>
+     </div>
+     <div>
+       <span>{transcript}</span>
+       <Say
+        speak="Hi everyone"
+        />
+     </div>
+   </div>
+ );
+};
 
 export default App;
-
-
